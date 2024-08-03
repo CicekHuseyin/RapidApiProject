af030d8ec6ef6a86a308cec263433569815e31b7
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-08-03 15:04:59 +0300

    Proje üzerinde Exchange2 controller ında sadece TL ye karşılık gelen Döviz kurlarını rapidapi ile çektik.

---
+++ RapidApiProject/Controllers/Exchange2Controller.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiProject.Models;
+
+namespace RapidApiProject.Controllers
+{
+    public class Exchange2Controller : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?locale=en-gb&currency=TRY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a18599a791msh5bf42e8586c65a7p1677fejsne99b31359a09" },
+        { "x-rapidapi-host", "booking-com.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
+        }
+    }
+}
